d5404d2f2f3e3475de2031265d93c0667591be2a
xiaobai <xiaobai@asyou.me>
2015-12-11 14:54:31 +0800

    http

--- pkg/handler/UserHander.go
+++ pkg/handler/UserHander.go
@@ -9,8 +9,8 @@ type UserHandler struct {
 }
 
 func (this UserHandler) Register(u string) (r string, err error) {
-	fmt.Print("Register()\n")
-	return "Register result2", nil
+	fmt.Print("Register()\n" + u)
+	return "Register result2 " + u, nil
 }
 
 func (this UserHandler) Login(key, pwd string) (r string, err error) {
--- pkg/http_ser/main.go
+++ pkg/http_ser/main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"github.com/asyoume/Auth/pkg/handler"
 	"github.com/labstack/echo"
 	mw "github.com/labstack/echo/middleware"
 	"github.com/rs/cors"
 	"net/http"
 )
+
+var uhander = handler.UserHandler{}
+
+func UserRegister(c *echo.Context) error {
+	r, err := uhander.Register(c.Query("u"))
+	if err == nil {
+		return c.String(http.StatusOK, r)
+	} else {
+		return c.String(http.StatusOK, "err")
+	}
+}
+
+func UserLogin(c *echo.Context) error {
+	r, err := uhander.Register(c.Value("u").(string))
+	if err == nil {
+		return c.String(http.StatusOK, r)
+	} else {
+		return c.String(http.StatusOK, "err")
+	}
+}
 
 func main() {
 	e := echo.New()
@@ -15,5 +36,7 @@ func main() {
 	e.Use(cors.Default().Handler)
 
 	// Routes
-	e.Get("/", hello)
+	e.Get("/Register", UserRegister)
+	e.Get("/Register", UserRegister)
+	e.Run(":9091")
 }
